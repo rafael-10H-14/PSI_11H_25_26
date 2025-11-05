8d1797f56b369359a063cdd9ce961092150f663d
Rafael Ferraz da Silva <a7660@esap.edu.pt>
2025-11-05 13:20:56 +0000

    M9_Aula02_Retângulo

--- M9_Ex2_Retângulo/Program.cs
+++ M9_Ex2_Retângulo/Program.cs
@@ -10,7 +10,19 @@ namespace M9_Ex2_Retângulo
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Digite a largura do retângulo > ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a altura do retângulo > ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+            Retângulo retângulo = new Retângulo();
+            retângulo.Largura = largura;
+            retângulo.Altura = altura;
+            Console.WriteLine("Área = " + retângulo.CalcularArea());
+            Console.WriteLine("Perímetro = " + retângulo.CalcularPerimetro());
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
         }
     }
 }

8350fd460ddfb8e715c701c6111d6bed38f5448c
Rafael Ferraz da Silva <a7660@esap.edu.pt>
2025-11-05 13:20:18 +0000

    M9_Aula02_Retângulo

---
+++ M9_Ex2_Retângulo/Retângulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M9_Ex2_Retângulo
+{
+    internal class Retângulo
+    {
+        public double Largura;
+        public double Altura;
+        public double CalcularArea()
+        {
+            return Largura * Altura;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}

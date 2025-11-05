d72140eab8ae241e03b5fabe2f140bd98b528fcc
Rafael Ferraz da Silva <a7660@esap.edu.pt>
2025-11-05 12:09:43 +0000

    Aula01_M9_Carro

---
+++ M9_Ex1_Carro/Carro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M9_Ex1_Carro
+{
+    internal class Carro
+    {
+        // Atributos
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public double Cilindrada { get; set; }
+        public double Velocidade { get; private set; }
+        // Construtor
+        public Carro(string marca, string modelo, string cor, double velocidade, double cilindrada)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cor = cor;
+            Cilindrada = cilindrada;
+            Velocidade = velocidade;
+        }
+        public void Acelerar(double incremento)
+        {
+            Velocidade += incremento;
+            Console.WriteLine($"O carro acelerou {incremento} km/h. Velocidade atual: {Velocidade} km/h.");
+        }
+        public void Parar(double decremento)
+        {
+            Velocidade -= decremento;
+            if (Velocidade < 0)
+            {
+                Velocidade = 0;
+            }
+            Console.WriteLine($"O carro desacelerou {decremento} km/h. Velocidade atual: {Velocidade} km/h.");
+        }
+        public void Pintar(string novaCor)
+        {
+            Cor = novaCor;
+            Console.WriteLine($"O carro foi pintado de {novaCor}.");
+        }
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("Informações do Carro:");
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Cor: {Cor}");
+            Console.WriteLine($"Cilindrada: {Cilindrada} L");
+            Console.WriteLine($"Velocidade Atual: {Velocidade} km/h");
+        }
+    }
+}
---
+++ M9_Ex1_Carro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M9_Ex1_Carro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao programa de gerenciamento de carros!");
+            Carro meuCarro = new Carro("Toyota", "Corolla", "Prata", 0, 1.8);
+            meuCarro.ExibirInformacoes();
+            meuCarro.Acelerar(50);
+            meuCarro.Parar(20);
+            meuCarro.Pintar("Vermelho");
+            meuCarro.ExibirInformacoes();
+            Console.WriteLine("Obrigado por usar o programa de gerenciamento de carros!");
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}

2217ba8875c4a267adc5a3ddb9cfa18eb6d5408e
Rafael Ferraz da Silva <a7660@esap.edu.pt>
2025-12-12 14:44:31 +0000

    Aula01_M10_Ex01

---
+++ M10_Ex01_Veículos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static M10_Ex01_Veículos.Veiculos;
+
+
+namespace M10_Ex01_Veículos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}

d9d2fd1409aea90eeb641a78298fd8a0761870cf
Rafael Ferraz da Silva <a7660@esap.edu.pt>
2025-12-12 15:22:08 +0000

    M10_Ex02_Aula01_Animais

---
+++ M10_Ex02_Animais/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10_Ex02_Animais
+{
+    public class Animal
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public Animal(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine($"{Nome} emite um som.");
+        }
+    }
+    public class Cao : Animal
+    {
+        public Cao(string nome, int idade) : base(nome, idade) { }
+        public override void EmitirSom()
+        {
+            Console.WriteLine("O cão ladra: Au Au!");
+        }
+    }
+    public class Gato : Animal
+    {
+        public Gato(string nome, int idade) : base(nome, idade) { }
+        public override void EmitirSom()
+        {
+            Console.WriteLine("O gato mia: Miau!");
+        }
+    }
+}
---
+++ M10_Ex02_Animais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10_Ex02_Animais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var animais = new List<Animal>
+            {
+                new Cao("Rex", 5),
+                new Gato("Mimi", 3),
+                new Cao("Bolt", 2),
+                new Gato("Whiskers", 1)
+            };
+            foreach (var animal in animais)
+            {
+                animal.EmitirSom();
+            }
+            Console.WriteLine("\nPrima qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
